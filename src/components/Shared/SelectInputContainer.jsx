b24008fef6477f3bba27a17dad05a2c753c35722
ShayanJamil519 <shayanjamil500@gmail.com>
2024-01-11 20:26:45 +0500

    addmember

--- src/components/Shared/SelectInputContainer.jsx
+++ src/components/Shared/SelectInputContainer.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import { FaCaretDown } from "react-icons/fa";
 
 const SelectInputContainer = ({ text, dropDownArray }) => {
   return (
     <div className="flex flex-col gap-2">
       <p className="text-[18px]">{text}</p>
 
-      <select
-        className={`w-full py-3 text-xs pl-3 rounded-full border-2 text-[#8F9BBA] bg-[#FFF] border-gray-300 focus:outline-none`}
-      >
-        <option value="">Please select</option>
-        {dropDownArray.map((option, index) => {
-          return (
-            <option value={option} key={index}>
-              {option}
-            </option>
-          );
-        })}
-      </select>
+      <div className="relative">
+        <select
+          className={`w-full py-3 text-xs pl-3 relative z-20 inline-flex appearance-none  pr-8 rounded-full border-2 text-[#8F9BBA] bg-[#FFF] border-gray-300 focus:outline-none`}
+        >
+          <option value="">Please select</option>
+          {dropDownArray.map((option, index) => {
+            return (
+              <option value={option} key={index}>
+                {option}
+              </option>
+            );
+          })}
+        </select>
+        <span className="absolute top-1/2 right-3 z-10 -translate-y-1/2">
+          <FaCaretDown className="text-[#A3AED0]" />
+        </span>
+      </div>
     </div>
   );
 };
--- src/pages/AddMember.jsx
+++ src/pages/AddMember.jsx
@@ -13,22 +13,22 @@ const AddMember = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="bg-[#111C44] rounded-3xl w-[80%] mx-auto mt-20">
+      <form onSubmit={handleSubmit} className="mx-auto w-[90%]   my-12">
+        <div className="bg-[#111C44] rounded-3xl ">
           <p className="px-7 py-6 text-left text-2xl font-bold text-white uppercase tracking-wider">
             회원관리
           </p>
           {/* <hr /> */}
           <div className="border-b-[1px] border-[#ffffff1a] bg-[#111C44]"></div>
           <div className="py-6">
-            <div className="py-6 px-7 grid grid-cols-3 gap-4 text-white">
-              <InputContainer text="이름" />
-              <InputContainer text="비밀번호" />
-              <InputContainer text="비밀번호 확인" />
+            <div className="py-6 px-10 grid grid-cols-3 gap-16 text-white">
+              <InputContainer text="이름" placeholder="Kim" />
+              <InputContainer text="비밀번호" placeholder="h12345" />
+              <InputContainer text="비밀번호 확인" placeholder="2023.11.04" />
             </div>
 
-            <div className="py-6 px-7 grid grid-cols-3 gap-4 text-white">
-              <InputContainer text="이메일" />
+            <div className="py-6 px-10 grid grid-cols-3 gap-16 text-white">
+              <InputContainer text="이메일" placeholder="test@gmail.com" />
               <SelectInputContainer
                 text="회원등급"
                 dropDownArray={memebership_level}
@@ -37,7 +37,7 @@ const AddMember = () => {
           </div>
         </div>
 
-        <div className="w-[80%] flex justify-end mx-auto mt-5">
+        <div className=" flex justify-end mx-auto mt-5">
           <Button text="회원생성" />
         </div>
       </form>
