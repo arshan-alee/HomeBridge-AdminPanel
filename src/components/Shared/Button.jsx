eb64e546e7a347e787f694c57680deae081e9e23
ShayanJamil519 <shayanjamil500@gmail.com>
2024-01-11 20:18:30 +0500

    memberdetails

--- src/components/Shared/Button.jsx
+++ src/components/Shared/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ text }) => {
   return (
-    <button className="bg-[#111C44] text-white px-10 py-4 font-bold rounded-3xl">
+    <button className="bg-[#111C44] text-white px-10 py-4 font-bold rounded-[20px]">
       회원생성
     </button>
   );
--- src/pages/MemberDetails.jsx
+++ src/pages/MemberDetails.jsx
@@ -7,35 +7,34 @@ const MemberDetails = () => {
   const memebership_level = ["일반회원", "일반회원", "일반회원", "일반회원"];
 
   return (
-    <>
-      <div className="bg-[#111C44] rounded-3xl w-[80%] mx-auto mt-20">
+    <form className="w-[90%]  my-12 mx-auto">
+      <div className="bg-[#111C44] rounded-3xl ">
         <p className="px-7 py-6 text-left text-2xl font-bold text-white uppercase tracking-wider">
           회원관리
         </p>
         {/* <hr /> */}
         <div className="border-b-[1px] border-[#ffffff1a] bg-[#111C44]"></div>
         <div className="py-6">
-          <div className="py-6 px-7 grid grid-cols-3 gap-4 text-white">
-            <InputContainer text="이름" />
-            <InputContainer text="비밀번호" />
-            <InputContainer text="비밀번호 확인" />
+          <div className="py-6 px-10 grid grid-cols-3 gap-16 text-white">
+            <InputContainer text="이름" placeholder="Kim" />
+            <InputContainer text="비밀번호" placeholder="h12345" />
+            <InputContainer
+              text="비밀번호 확인"
+              placeholder="2023.11.04  14:32"
+            />
           </div>
 
-          <div className="py-6 px-7 grid grid-cols-3 gap-4 text-white">
-            <InputContainer text="이메일" />
-            <SelectInputContainer
-              text="회원등급"
-              dropDownArray={memebership_level}
-            />
+          <div className="py-6 px-10 grid grid-cols-3 gap-16 text-white">
+            <InputContainer text="이메일" placeholder="test@gmail.com" />
+            <InputContainer text="회원등급" placeholder="일반회원" />
           </div>
         </div>
       </div>
-
-      <div className="w-[80%] flex justify-end mx-auto mt-5 gap-5">
+      <div className="flex justify-end mx-auto mt-5 gap-5">
         <Button text="회원수정" />
         <Button text="회원삭제" />
       </div>
-    </>
+    </form>
   );
 };
 
