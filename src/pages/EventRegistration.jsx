a299bdd8078c4178876b6d48bfd7ed7e0985ef04
ShayanJamil519 <shayanjamil500@gmail.com>
2024-01-12 15:28:55 +0500

    event_resgistration in process

--- src/pages/EventRegistration.jsx
+++ src/pages/EventRegistration.jsx
@@ -15,13 +15,14 @@ const EventRegistration = () => {
   return (
     <>
       <form onSubmit={handleSubmit} className="mx-auto w-[90%]   my-12">
+        {/* Top */}
         <div className="bg-[#111C44] rounded-3xl">
           <p className="px-10 py-6 text-left text-2xl font-bold text-white uppercase tracking-wider">
             Event 공고
           </p>
           {/* <hr /> */}
           <div className="border-b-[1px] border-[#ffffff1a] bg-[#111C44]"></div>
-          <div className="pb-6">
+          <div className="pb-20">
             <div className="py-6 px-10 grid gap-16 text-white">
               <InputContainer
                 text="상품소개"
@@ -31,18 +32,31 @@ const EventRegistration = () => {
 
             <div className="grid py-6 px-10 text-white">
               <Textarea
-                text="메시지"
-                placeholder="hello"
-                height="200px"
-                rounded="50px"
+                text="Event Information"
+                placeholder=""
+                height="300px"
+                rounded="10px"
+              />
+            </div>
+            <div className="grid py-6 px-10 text-white">
+              <Textarea
+                text="Product information"
+                placeholder=""
+                height="300px"
+                rounded="10px"
               />
             </div>
           </div>
         </div>
+        {/* Schdeule Button */}
+
+        <button className="bg-[#111C44] rounded-3xl mt-4 flex w-full justify-center items-center gap-4 text-[32px] text-[#fff] font-bold py-6 px-10">
+          일정 추가하기
+          <img src="/images/add_icon.png" alt="logo" />
+        </button>
 
         <div className="flex justify-end gap-7 mt-5">
-          <Button text="신청서수정" />
-          <Button text="신청서삭제" />
+          <Button text="공고등록" />
         </div>
       </form>
     </>
