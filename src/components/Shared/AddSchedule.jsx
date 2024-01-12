a2ad55389ab1217e293933f19f4deb8750671427
ShayanJamil519 <shayanjamil500@gmail.com>
2024-01-12 16:58:05 +0500

    event_resgistration completed

--- src/components/Shared/AddSchedule.jsx
+++ src/components/Shared/AddSchedule.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import InputContainer from "./InputContainer";
 import Textarea from "./Textarea";
 import UploadInput from "./UploadInput";
 
 const AddSchedule = ({ removeSchedule }) => {
+  const [daySchedules, setDaySchedules] = useState([]);
+
+  const addDaySchedule = () => {
+    setDaySchedules([...daySchedules, {}]);
+  };
+
   return (
     <div className="bg-[#111C44] rounded-3xl mt-4 pb-16">
       <div className="px-10 py-6 flex justify-between items-center">
@@ -29,19 +35,26 @@ const AddSchedule = ({ removeSchedule }) => {
       </div>
 
       {/* Add Day Schedule */}
-
-      <div className="grid items-end grid-cols-4 gap-8 px-10 mt-7">
-        <div className="col-span-1 text-[#fff] flex flex-col gap-10">
-          <InputContainer text="상세일정" placeholder="서울" />
-          <UploadInput />
+      {daySchedules.map((schedule, index) => (
+        <div
+          key={index}
+          className="grid items-end grid-cols-4 gap-8 px-10 mt-7"
+        >
+          <div className="col-span-1 text-[#fff] flex flex-col gap-10">
+            <InputContainer text="상세일정" placeholder="서울" />
+            <UploadInput />
+          </div>
+          <div className="col-span-3">
+            <Textarea text="" placeholder="" height="300px" rounded="10px" />
+          </div>
         </div>
-        <div className="col-span-3">
-          <Textarea text="" placeholder="" height="300px" rounded="10px" />
-        </div>
-      </div>
+      ))}
 
       {/* Schdeule Button */}
-      <button className="bg-[#7551FF] w-[90%] mx-auto rounded-3xl mt-8 flex  justify-center items-center gap-4 text-[32px] text-[#fff] font-bold py-6 ">
+      <button
+        onClick={addDaySchedule}
+        className="bg-[#7551FF] w-[90%] mx-auto rounded-3xl mt-8 flex  justify-center items-center gap-4 text-[32px] text-[#fff] font-bold py-6 "
+      >
         Day1 일정 추가하기
         <img src="/images/add_icon.png" alt="logo" />
       </button>
