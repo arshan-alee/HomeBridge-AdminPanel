c52aabb81ad028827bf1f64d0f169ac415e54b5e
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2024-02-04 22:32:17 +0500

    job and house flow done

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [Loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const checkUserToken = () => {
+    const Info = localStorage.getItem("info");
+    const userToken = JSON.parse(Info)?.token;
+
+    if (!userToken || userToken === "undefined") {
+      setIsLoggedIn(false);
+      // return navigate("/");
+    }
+
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    checkUserToken();
+  }, []);
   return (
     <div className="flex justify-between items-center px-16 h-full  shadow-sm border-b-[1px] border-gray-300   ">
       <h1 className="text-lg font-semibold">HOMEBRIDGE</h1>
       <div className="flex justify-start items-center gap-4">
         <img src="/images/user circle.png" alt="logo" />
-        <p className="text-lg">Login</p>
+        {isLoggedIn ? (
+          <p
+            className="text-lg cursor-pointer"
+            onClick={() => {
+              localStorage.removeItem("info");
+              navigate("/");
+            }}
+          >
+            Logout
+          </p>
+        ) : (
+          <p className="text-lg cursor-pointer">Login</p>
+        )}
       </div>
     </div>
   );
--- src/components/Shared/UploadRegisterationImage.jsx
+++ src/components/Shared/UploadRegisterationImage.jsx
@@ -1,34 +1,60 @@
 import React, { useState } from "react";
+import { uploadImage } from "../../utils/helper";
+import RequestLoader from "./RequestLoader";
 
 const UploadRegisterationImage = ({ setUploadImages }) => {
-  const handleImageChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      let img = e.target.files[0];
-      //   setSelectedImage(URL.createObjectURL(img));
-      setUploadImages((prevData) => [...prevData, URL.createObjectURL(img)]);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleImageChange = async (e) => {
+    try {
+      setError(null);
+      setLoading(true);
+
+      if (e.target.files && e.target.files[0]) {
+        let img = e.target.files[0];
+        setSelectedImage(URL.createObjectURL(img));
+
+        // Call uploadImage function
+        const imageUrl = await uploadImage(img);
+
+        setUploadImages((prevData) => [...prevData, imageUrl]);
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error.message);
+      setError("Error uploading image");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="h-[170px] w-[180px] grid place-items-center bg-[#fff] rounded-md mr-5">
-      <label
-        htmlFor="file-upload"
-        className="w-[70px] h-[70px] grid place-content-center bg-[#1B254B] cursor-pointer rounded-full"
-      >
-        <img src="/images/upload.png" alt="Upload Icon" />
-      </label>
-      <input
-        type="file"
-        onChange={handleImageChange}
-        id="file-upload"
-        style={{ display: "none" }}
-      />
-      <label
-        htmlFor="file-upload"
-        className="text-[18px] font-bold text-[#1B254B] -mt-7 cursor-pointer"
-      >
-        숙소이미지 등록
-      </label>
+      {loading ? (
+        <RequestLoader size="large" />
+      ) : (
+        <>
+          <label
+            htmlFor="file-upload"
+            className="w-[70px] h-[70px] grid place-content-center bg-[#1B254B] cursor-pointer rounded-full"
+          >
+            <img src="/images/upload.png" alt="Upload Icon" />
+          </label>
+          <input
+            type="file"
+            onChange={handleImageChange}
+            id="file-upload"
+            style={{ display: "none" }}
+          />
+          <label
+            htmlFor="file-upload"
+            className="text-[18px] font-bold text-[#1B254B] -mt-7 cursor-pointer"
+          >
+            숙소이미지 등록
+          </label>
+        </>
+      )}
     </div>
   );
 };
