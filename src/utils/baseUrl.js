e2c488a38038ba819d46035e34d387d88b58075d
ShayanJamil519 <shayanjamil500@gmail.com>
2024-02-23 12:13:24 +0500

    Base URL Added

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -3,7 +3,8 @@ const environment = "production";
 
 let baseUrl;
 if (environment === "production") {
-  baseUrl = "https://black-panda-slip.cyclic.app";
+  // baseUrl = "https://black-panda-slip.cyclic.app";
+  baseUrl = "https://home-bridge-backend-0e155295c3cc.herokuapp.com";
 } else {
   baseUrl = "http://localhost:5000";
 }
