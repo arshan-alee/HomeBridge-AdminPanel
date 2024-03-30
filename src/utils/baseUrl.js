160340ef05fd6d54f3c4c23e5806071ae8fa5d11
Mir Arshan Ali <109890622+arshan-alee@users.noreply.github.com>
2024-03-30 22:53:26 +0500

    Update baseUrl.js

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -3,7 +3,7 @@ const environment = "production";
 
 let baseUrl;
 if (environment === "production") {
-  baseUrl = "https://homebridge-backend-c67d8f57f4c3.herokuapp.com";
+  baseUrl = "https://black-panda-slip.cyclic.app";
 } else {
   baseUrl = "http://localhost:5000";
 }
